e8384ea8eca1bed2f3e4f046493b8cbf068d4318
rj-stock <rongji_h@hotmail.com>
2023-05-18 11:23:16 +0800

    添加自动编译并发布到通达信的 bash 脚本

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 RJ 的通达信 dll 编程。
 
+## 自动编译并发布到通达信
+
+```shell
+./build.sh "D:/path-to-install-tdx"
+```
+
+执行此 bash 脚本将自动编译 dll 并复制到 `TDX/T0002/dlls/` 目录下，第一个参数需指定为通达信的安装路径（不指定默认为 `c:/new_tdx`）。
+
 ## MinGW 编译说明
 
 到 <https://github.com/niXman/mingw-builds-binaries/releases> 下载 MinGW 的 32 位版，其名称类似为 `i686-12.2.0-release-win32-dwarf-msvcrt-rt_v10-rev2.7z`，解压后将 `mingw32/bin` 目录配置到系统环境变量 `Path` 内即可在命令行直接使用 `g++` 来编译 `C++` 源代码。
@@ -10,6 +18,7 @@ RJ 的通达信 dll 编程。
 
 ```shell
 # 使用 MinGW32 的 g++ 编译测试成功，-m32 参数设与不设编译出来的 dll，放到通达信实测都 OK
+g++ -shared -o build/TestPluginTCale.dll StdAfx.cpp TCalcFuncSets.cpp TestPluginTCale.cpp
 g++ -m32 -shared -o build/TestPluginTCale.dll StdAfx.cpp TCalcFuncSets.cpp TestPluginTCale.cpp
 ```
 
---
+++ build.sh
@@ -0,0 +1,44 @@
+# 编译生成 dll 并复制到 TDX/T0002/dlls/ 的脚本
+
+# dll 文件名称
+dllFile="TestPluginTCale.dll"
+
+# 创建 build 目录
+if [ ! -d "build" ]; then
+  echo "创建 build 目录"
+  mkdir -p "build"
+fi
+
+# 执行编译
+echo "开始编译生成 dll ..."
+if g++ -shared -o "build/$dllFile" "StdAfx.cpp" "TCalcFuncSets.cpp" "TestPluginTCale.cpp"; then
+  echo "编译成功，生成文件 build/$dllFile"
+
+  # 第 1 个参数可以指定 TDX 的安装路径
+  if [ ! -n "$1" ]; then
+    tdxDir="c:/new_tdx"
+  else
+    tdxDir=$1
+  fi
+
+  # 判断 TDX 是否存在
+  if [ ! -d "$tdxDir" ]; then
+    echo "通达信安装路径 “$tdxDir” 不存在，请指定为正确的安装路径"
+    exit 1
+  fi
+
+  # 创建 TDX/T0002/dlls 目录
+  if [ ! -d "$tdxDir/T0002/dlls" ]; then
+    mkdir $tdxDir/T0002/dlls
+    echo "创建 \$TDX/T0002/dlls 目录"
+  fi
+
+  # 复制 dll
+  if cp -f "build/$dllFile" "$tdxDir/T0002/dlls/"; then
+    echo "已复制 build/$dllFile 到 \$TDX/T0002/dlls/$dllFile，启动 TDX 即可使用此 dll"
+  else
+    echo "复制 dll 到 \$TDX/T0002/dlls 失败，通常如果 TDX 正在运行使用此 dll 就会导致复制失败，此时先关闭 TDX 再执行此脚本，通常都能解决问题"
+  fi
+else
+  echo "编译失败"
+fi
