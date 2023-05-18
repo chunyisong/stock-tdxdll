a30c7050ccd2966c71407e3bb561c10549e236d8
rj-stock <rongji_h@hotmail.com>
2023-05-18 11:36:35 +0800

    重新格式化所有源码-两空格缩进

--- TCalcFuncSets.h
+++ TCalcFuncSets.h
@@ -2,15 +2,11 @@
 #define __TCALC_FUNC_SETS
 #include "PluginTCalcFunc.h"
 
-
-
-
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif //__cplusplus
-	__declspec(dllexport) BOOL RegisterTdxFunc(PluginTCalcFuncInfo** pFun);
+  __declspec(dllexport) BOOL RegisterTdxFunc(PluginTCalcFuncInfo **pFun);
 #ifdef __cplusplus
 }
 #endif //__cplusplus
--- TestPluginTCale.cpp
+++ TestPluginTCale.cpp
@@ -3,11 +3,9 @@
 
 #include "stdafx.h"
 
-BOOL APIENTRY DllMain( HANDLE hModule, 
-                       DWORD  ul_reason_for_call, 
-                       LPVOID lpReserved
-					 )
+BOOL APIENTRY DllMain(HANDLE hModule,
+                      DWORD ul_reason_for_call,
+                      LPVOID lpReserved)
 {
-    return TRUE;
+  return TRUE;
 }
-
